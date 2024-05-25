30efd78e1466ef2ac9d55c260db876a45c1d7bfc
Md Mubarak Hossain <hossainmubarak477@gmai.com>
2024-05-25 18:54:57 +0600

    updated

--- src/documentations/default/home/Banner.js
+++ src/documentations/default/home/Banner.js
@@ -31,43 +31,43 @@ const Banner = () => {
                                                             Buy Your Fashion See You More Charming</div>
                                                     </div>
                                                     <div
-                                                        className="flex flex-col lg:flex-row space-x-2 space-y-3 md:space-x-6   w-full items-center justify-center ">
+                                                        className="flex md:flex-row space-x-2 space-y-3 md:space-x-6   w-full items-center justify-center ">
                                     
-                                                        <div className="lg:w-40 w-64 h-40  overflow-hidden rounded-xl ">
+                                                        <div className="md:w-40 w-64 h-40  overflow-hidden rounded-xl ">
                                                             <img src={mobile[imag]?.picture}
                                     
                                                             alt={mobile[imag]?.brand} className="" />
                                                         </div>
-                                                        <div className="flex flex-row lg:flex-col space-x-3 space-y-6 items-center justify-center">
-                                                            <div className="w-32 lg:w-40 h-32  overflow-hidden rounded-xl ">
+                                                        <div className="flex flex-row md:flex-col space-x-3 space-y-6 items-center justify-center">
+                                                            <div className="w-32 md:w-40 h-32  overflow-hidden rounded-xl ">
                                                                 <img src={mobile[imag]?.picture}
                                     
                                                                 alt={mobile[imag]?.brand} className="" />
                                                             </div>
-                                                            <div className="w-32 lg:w-40 h-48  overflow-hidden rounded-xl ">
+                                                            <div className="w-32 md:w-40 h-48  overflow-hidden rounded-xl ">
                                                                 <img src={mobile[imag]?.picture}
                                     
                                                                 alt={mobile[imag]?.brand} className="" />
                                                             </div>
                                                         </div>
-                                                        <div className="lg:w-60 w-64 h-96  overflow-hidden rounded-xl ">
+                                                        <div className="md:w-60 w-64 h-96  overflow-hidden rounded-xl ">
                                                             <img src={mobile[imag]?.picture}
                                     
                                                             alt={mobile[imag]?.brand} className="" />
                                                         </div>
-                                                        <div className="flex flex-row lg:flex-col space-x-3 space-y-6 items-center justify-center ">
-                                                            <div className="w-32 lg:w-40 h-48  overflow-hidden rounded-xl ">
+                                                        <div className="flex flex-row md:flex-col space-x-3 space-y-6 items-center justify-center ">
+                                                            <div className="w-32 md:w-40 h-48  overflow-hidden rounded-xl ">
                                                                 <img src={mobile[imag]?.picture}
                                     
                                                                 alt={mobile[imag]?.brand} className="" />
                                                             </div>
-                                                            <div className="w-32 lg:w-40 h-32  overflow-hidden rounded-xl ">
+                                                            <div className="w-32 md:w-40 h-32  overflow-hidden rounded-xl ">
                                                                 <img src={mobile[imag]?.picture}
                                     
                                                                 alt={mobile[imag]?.brand} className="" />
                                                             </div>
                                                         </div>
-                                                        <div className="lg:w-40 w-64 h-40  overflow-hidden rounded-xl ">
+                                                        <div className="md:w-40 w-64 h-40  overflow-hidden rounded-xl ">
                                                             <img src={mobile[imag]?.picture}
                                     
                                                             alt={mobile[imag]?.brand} className="" />
